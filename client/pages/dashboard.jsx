aa9f7048b7f10ff7e5c3d5c3ee8564e71adf8555
Jose Daniel <josedanq100@gmail.com>
2019-03-26 21:56:25 -0400

    Add new view

---
+++ client/pages/dashboard.jsx
@@ -0,0 +1,5 @@
+const Dashboard = () => (
+  <h1>This is the dashboard</h1>
+)
+
+export default Dashboard
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,5 +1,9 @@
 const Index = () => (
-  <h1>Hello from Next.js</h1>
+  <div>
+    <h1>Welcome to Assist.me</h1>
+    <h2>Created by 3Dmensional Group</h2>
+
+  </div>
 )
 
 export default Index
